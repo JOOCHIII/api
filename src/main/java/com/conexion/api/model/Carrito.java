2f8d6b1e44201443f4b2e1c279dfaee93e5cf1f9
Jorge Diaz <jorgediazmarr@gmail.com>
2025-05-24 19:12:28 +0200

    carrito

--- src/main/java/com/conexion/api/model/Carrito.java
+++ src/main/java/com/conexion/api/model/Carrito.java
@@ -1,5 +1,70 @@
 package com.conexion.api.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "carrito")
 public class Carrito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", nullable = false)
+    private Productos producto;
+
+    private int cantidad;
+
+    private LocalDateTime fechaAgregado;
+
+    public Carrito() {
+        this.fechaAgregado = LocalDateTime.now();
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Productos getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Productos producto) {
+		this.producto = producto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public LocalDateTime getFechaAgregado() {
+		return fechaAgregado;
+	}
+
+	public void setFechaAgregado(LocalDateTime fechaAgregado) {
+		this.fechaAgregado = fechaAgregado;
+	}
 
 }
