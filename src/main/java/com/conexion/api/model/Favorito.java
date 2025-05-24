322a537504024e5d465c85bb4f7599f1b69189fa
Jorge Diaz <jorgediazmarr@gmail.com>
2025-05-24 14:30:06 +0200

    cambio long id producto2

--- src/main/java/com/conexion/api/model/Favorito.java
+++ src/main/java/com/conexion/api/model/Favorito.java
@@ -10,14 +10,20 @@ public class Favorito {
     private FavoritoId id;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @MapsId("idUsuario")  // mapea la parte idUsuario del composite key
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @MapsId("idProducto")  // mapea la parte idProducto del composite key
     @JoinColumn(name = "id_producto")
     private Productos producto;
 
     public Favorito() {}
 
-    public Favorito(FavoritoId id, Productos producto) {
+    public Favorito(FavoritoId id, Usuario usuario, Productos producto) {
         this.id = id;
+        this.usuario = usuario;
         this.producto = producto;
     }
 
@@ -29,6 +35,14 @@ public class Favorito {
         this.id = id;
     }
 
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
     public Productos getProducto() {
         return producto;
     }
@@ -37,3 +51,5 @@ public class Favorito {
         this.producto = producto;
     }
 }
+
+
