90ec8c64ded0e57e3fe3630cd586a9915b22cf02
kmyte <kmyteran2@hotmail.com>
2025-06-09 18:11:59 +0200

    Añadir validaciones

--- src/main/java/com/conexion/api/repository/UsuarioRepository.java
+++ src/main/java/com/conexion/api/repository/UsuarioRepository.java
@@ -7,12 +7,19 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	    Usuario findByUsuario(String usuario);
-	    Usuario findByCorreo(String correo);
-	    List<Usuario> findByOrigenApp(String origenApp);
-        boolean existsByUsuario(String usuario);
-        boolean existsByCorreo(String correo);
-        boolean existsByTelefono(String telefono);
-    }
+    Usuario findByUsuario(String usuario);
+    Usuario findByCorreo(String correo);
+    List<Usuario> findByOrigenApp(String origenApp);
+    boolean existsByUsuario(String usuario);
+    boolean existsByCorreo(String correo);
+    boolean existsByTelefono(String telefono);
+
+    // Nuevos métodos para exclusión por id (para actualizar)
+    boolean existsByNombrecompletoAndIdNot(String nombrecompleto, Long id);
+    boolean existsByCorreoAndIdNot(String correo, Long id);
+    boolean existsByTelefonoAndIdNot(String telefono, Long id);
+    boolean existsByUsuarioAndIdNot(String usuario, Long id);
+}
+
 
 
